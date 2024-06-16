c3a02b45e3baa9d362d4dfc97893a6712f5c419c
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-06-16 11:44:13 -0300

    add: tipos de dados

--- js/aula1/main.js
+++ js/aula1/main.js
@@ -1,1 +1,30 @@
-alert("Alerta de Hello!")
+// Strings
+const firstName = 'Alexandre'
+const lastName = 'Augusto'
+
+const names = "Alexandre,Gabriel,Rafael,Marcelo"
+
+console.log('Meu nome é ' + firstName + ' ' + lastName)
+console.log(`Meu nome é ${firstName.toUpperCase()} ${lastName.toLowerCase()}`)
+
+console.log(names.split(','))
+
+// Number
+const number = 5;
+console.log(number.toString())
+console.log(typeof number.toString())
+
+// Booleanos
+
+console.log(2 == 2)
+console.log(2 == 3)
+
+// Null & Undefined
+
+const x = null;
+const y = undefined;
+console.log(typeof x)
+console.log(typeof y)
+
+const list = [1,2,3,4,5];
+object = {name: 'Alexandre'}
