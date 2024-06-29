ffc693675938b06e4887e6da478d9d1ca5a9f811
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-06-29 10:33:15 -0300

    otimizando a estrutura do repositorio

---
+++ curso/Seção 0/tests.js
@@ -0,0 +1,23 @@
+console.log('João nasceu em 1984.');
+console.log('Em 2000 João conheceu Maria.');
+console.log('João casou-se com Maria em 2012.');
+console.log('Maria teve um filho com João em 2015.');
+console.log('O filho deles se chama Gabriel.');
+
+// function enviarForm(){
+//     const form = document.querySelector('.form');
+
+//     // Maneira 1
+//     // form.onsubmit = function(evento){
+//     //     evento.preventDefault();
+//     //     alert('Primeira maneira de enviar um form');
+//     //     console.log('Enviado');
+//     // };
+
+//     // Maneira 2
+//     function receberEventoForm(evento){
+        
+//     }
+
+//     form.addEventListener('submit', receberEventoForm)
+// }
