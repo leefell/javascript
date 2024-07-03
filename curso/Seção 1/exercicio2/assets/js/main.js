430345729a44f69235fa0f167576777b1b3586a1
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-07-02 21:25:00 -0300

    grammar: explicando código

--- curso/Seção 1/exercicio2/assets/js/main.js
+++ curso/Seção 1/exercicio2/assets/js/main.js
@@ -1,48 +1,38 @@
-const h1 = document.querySelector('.container h1');
-const data = new Date();
+const h1 = document.querySelector('.container h1'); // pega o elemento dentro de h1 e guarda na variavel
+const data = new Date(); // instancia uma nova data atual do sistema
 
 function getDia(numeroDia){
     const diasDaSemana = [
         'domingo', 'segunda-feira', 'terça-feira', 'quarta-feira', 
-        'quinta-feira', 'sexta-feira', 'sábado'];
+        'quinta-feira', 'sexta-feira', 'sábado']; // cria um array de 7 posições
         
-    return diasDaSemana[numeroDia]
+    return diasDaSemana[numeroDia] // retorna o array no elemento recebido 
 }
 
 function getMes(numeroMes){
     const meses = [
         'janeiro', 'fevereiro', 'março', 'abril', 'maio', 
         'junho', 'julho', 'agosto', 'setembro', 'outubro', 
-        'novembro', 'dezembro'];
+        'novembro', 'dezembro']; // cria um array de 12 posicoes
 
-    return meses[numeroMes]
+    return meses[numeroMes] // retorna o array no elemento recebido
 }
 
-function zeroAEsquerda(num){
-    return num >= 10 ? num : `0${num}`;
+function zeroAEsquerda(num){    
+    return num >= 10 ? num : `0${num}`; // formata o número adicionando 0 caso ele seria menor que 10
 }
 
 function criaData(data){
-    const diaSemana = data.getDay();
-    const numeroMes = data.getMonth();
+    const diaSemana = data.getDay(); // declara uma variavel que pega o dia da data que foi instanciada
+    const numeroMes = data.getMonth(); // declarar uma variavel que pega o mes da data que foi instanciada
 
-    const nomeDia = getDia(diaSemana);
-    const nomeMes = getMes(numeroMes); 
+    const nomeDia = getDia(diaSemana); // declara uma variavel que recebe o retorno da funcao getDia()
+    const nomeMes = getMes(numeroMes); // declara uma variavel que recebe o retorno da funcao getMes()
 
     return (
         `${nomeDia}, ${data.getDate()} de ${nomeMes}` +
         ` de ${data.getFullYear()} ${zeroAEsquerda(data.getHours())}:${zeroAEsquerda(data.getMinutes())}`
-    );
+    ); // retorna a data de maneira formatada
 }
 
-h1.innerHTML = criaData(data); 
-
-// Todo esse código acima pode ser simplificado com:
-// const h1 = document.querySelector('.container h1');
-// const data = new Date();
-// const opcoes = {
-//     dataStyle: 'full',
-//     timeStyle: 'short'
-// };
-
-// h1.innerHTML = data.toLocaleDateString('pt-BR', opcoes);
+h1.innerHTML = criaData(data);
