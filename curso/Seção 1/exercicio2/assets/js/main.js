1b100d1f8cfbf6e5732ff43ee905015aec8888a0
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-07-02 21:13:49 -0300

    ex data v2

--- curso/Seção 1/exercicio2/assets/js/main.js
+++ curso/Seção 1/exercicio2/assets/js/main.js
@@ -1,83 +1,21 @@
 const h1 = document.querySelector('.container h1');
 const data = new Date();
 
-function getDia(diaSemana){
-    let diaSemanaTexto;
-
-    switch(diaSemana){
-        case 0:
-            diaSemanaTexto = 'Domingo';
-            break;
-        case 1:
-            diaSemanaTexto = 'Segunda-Feira';
-            break;
-        case 2: 
-            diaSemanaTexto = 'Terca-Feira';
-            break; 
-        case 3:
-            diaSemanaTexto = 'Quarta-Feira';
-            break;
-        case 4:
-            diaSemanaTexto = 'Quinta-Feira';
-            break;
-        case 5:
-            diaSemanaTexto = 'Sexta-Feira';
-            break;
-        case 6:
-            diaSemanaTexto = 'Sábado';
-            break;
-        default:
-            diaSemanaTexto = 'Seu dia ta fora do range.';       
-    }
-
-    return diaSemanaTexto;
+function getDia(numeroDia){
+    const diasDaSemana = [
+        'domingo', 'segunda-feira', 'terça-feira', 'quarta-feira', 
+        'quinta-feira', 'sexta-feira', 'sábado'];
+        
+    return diasDaSemana[numeroDia]
 }
 
-function getMes(mes){
-    let mesTexto;
+function getMes(numeroMes){
+    const meses = [
+        'janeiro', 'fevereiro', 'março', 'abril', 'maio', 
+        'junho', 'julho', 'agosto', 'setembro', 'outubro', 
+        'novembro', 'dezembro'];
 
-    switch(mes){
-        case 0:
-            mesTexto = 'Janeiro';
-            break;
-        case 1:
-            mesTexto = 'Fevereiro';
-            break;
-        case 2: 
-            mesTexto = 'Março';
-            break; 
-        case 3:
-            mesTexto = 'Abril';
-            break;
-        case 4:
-            mesTexto = 'Maio';
-            break;
-        case 5:
-            mesTexto = 'Junho';
-            break;
-        case 6:
-            mesTexto = 'Julho';
-            break;
-        case 7:
-            mesTexto = 'Agosto';
-            break;    
-        case 8:
-            mesTexto = 'Setembro';
-            break;
-        case 9:
-            mesTexto = 'Outubro';
-            break;
-        case 10:
-            mesTexto = 'Novembro';
-            break;
-        case 11:
-            mesTexto = 'Dezembro';
-            break;    
-        default:
-            mesTexto = 'Seu mês ta fora do range.';       
-    }
-
-    return mesTexto;
+    return meses[numeroMes]
 }
 
 function zeroAEsquerda(num){
