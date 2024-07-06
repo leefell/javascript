65d7c2a7081ca159685b2dcb034355f84a5dc7df
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-07-06 18:55:08 -0300

    for classico

---
+++ curso/Seção 1/repetição/forClassico.js
@@ -0,0 +1,7 @@
+// Index           0       1      2        3
+const frutas = ['Maçã', 'Pêra', 'Uva', 'Banana'];
+
+console.log(frutas.length);
+for(let i = 0; i <= frutas.length; i++){
+    console.log(frutas[i]);
+}
