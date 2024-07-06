b59a2060c832ea2ed06f4ebf9f68d523c832597d
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-07-06 19:57:32 -0300

    comentando o código do exercício 3

--- curso/Seção 1/Exercícios Seção 1/exercicio3/assets/js/main.js
+++ curso/Seção 1/Exercícios Seção 1/exercicio3/assets/js/main.js
@@ -3,19 +3,22 @@ const elementos = [
     {tag: 'div', texto: 'A felicidade está nas pequenas coisas do dia a dia.'},
     {tag: 'footer', texto: 'Os desafios nos fazem crescer e evoluir.'},
     {tag: 'section', texto: 'O tempo é o bem mais precioso que temos, use-o com sabedoria.'},
-];
+]; // Instancia um array de de objetos em que cada um possui uma tag e um texto
 
-const container = document.querySelector('.container');
+// Instancia uma variavel que recebe o valor do container
+const container = document.querySelector('.container');  
+
+// Intancia uma variavel que cria uma div 
 const div = document.createElement('div');
 
-for(let i = 0; i < elementos.length; i++){
-    // Desestruturando o array
-    let {tag, texto} = elementos[i];
+for(let i = 0; i < elementos.length; i++){ // Percorre o Array
+    // Desestruturando o array em tag e texti
+    let {tag, texto} = elementos[i]; 
     let tagCriada = document.createElement(tag); // vai criar um elemento diferente a cada iteração
-    let textoCriado = document.createTextNode(texto);
+    let textoCriado = document.createTextNode(texto); // cria um nó de texto
 
-    tagCriada.appendChild(textoCriado);
-    div.appendChild(tagCriada);
+    tagCriada.appendChild(textoCriado); // adiciona o textoCriado na tag
+    div.appendChild(tagCriada); // adiciona a tag na div
 }
 
-container.appendChild(div);
+container.appendChild(div); // adiciona a div no container
