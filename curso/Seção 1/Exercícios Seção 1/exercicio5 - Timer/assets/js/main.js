746e1b3b28f939bef5cb13e39a03b1be4049b878
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-07-10 09:55:15 -0300

    refatorando timer e explicando

--- curso/Seção 1/Exercícios Seção 1/exercicio5 - Timer/assets/js/main.js
+++ curso/Seção 1/Exercícios Seção 1/exercicio5 - Timer/assets/js/main.js
@@ -1,39 +1,41 @@
+const relogio = document.querySelector('.relogio') // Pega quem ta na classe relogio
+let segundos = 0; // inicia a variavel global segundos
+let timer; // declara um timer;
+
 function getTimeFromSeconds(seconds){
-    const date = new Date(seconds * 1000);
-    return date.toLocaleTimeString('pt-BR', {
+    // Instancia uma data que recebe como argumento os segundos * 1000 para tornar milisegundos
+    const date = new Date(seconds * 1000); 
+    return date.toLocaleTimeString('pt-BR', { // formata a data
         hour12:false,
         timeZone: 'GMT'
     });
 }
 
-const relogio = document.querySelector('.relogio')
-const iniciar = document.querySelector('.iniciar')
-const pausar = document.querySelector('.pausar')
-const zerar = document.querySelector('.zerar')
-let segundos = 0;
-let timer;
-
 function iniciaRelogio(){
-    timer = setInterval(function(){
+    timer = setInterval(function(){ 
         segundos++;
         relogio.innerHTML = getTimeFromSeconds(segundos);
     }, 1000); // A cada um segundo soma 1 na variavel
 }
 
-iniciar.addEventListener('click', function(evento){
-    relogio.classList.remove('pausado'); // tira a cor vermelha do timer;
-    clearInterval(timer); // Não criar dois timers na mesma página, cada click zera e iniciar um timer
-    iniciaRelogio();
-});
+document.addEventListener('click', function(evento){ // Adiciona o evento de click
+    const elemento = evento.target; // guarda o target que vem do evento em uma variavel
 
-pausar.addEventListener('click', function(evento){
-    relogio.classList.add('pausado');
-    clearInterval(timer);
-});
+    if(elemento.classList.contains('iniciar')){ // verifica se o tem a classe 'iniciar' no target
+        relogio.classList.remove('pausado'); // tira a cor vermelha do timer;
+        clearInterval(timer); // Não criar dois timers na mesma página, cada click zera e iniciar um timer
+        iniciaRelogio();
+    }
 
-zerar.addEventListener('click', function(evento){
-    relogio.classList.remove('pausado');
-    clearInterval(timer);
-    relogio.innerHTML = '00:00:00';
-    segundos = 0;
+    if(elemento.classList.contains('pausar')){ // verifica se o tem a classe 'pausar' no target
+        relogio.classList.add('pausado');
+        clearInterval(timer);
+    }
+
+    if(elemento.classList.contains('zerar')){ // verifica se o tem a classe 'zerar' no target
+        relogio.classList.remove('pausado');
+        clearInterval(timer);
+        relogio.innerHTML = '00:00:00';
+        segundos = 0;a
+    }
 });
