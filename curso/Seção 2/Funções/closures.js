dad5cbece3feb3bd80182edf8f8206e1e4b34719
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-07-12 10:11:46 -0300

    outro exemplo de closure

--- curso/Seção 2/Funções/closures.js
+++ curso/Seção 2/Funções/closures.js
@@ -10,3 +10,25 @@ function retornaFuncao(nome){
 const funcao = retornaFuncao('Fulano'); // Closure 1 Nome => Fulano
 const funcao2 = retornaFuncao('Ciclano'); // Closure 2 Nome => Ciclano
 console.log(funcao(), funcao2());
+
+function criaContador(){
+    let contador = 0;
+
+    return function(){
+        contador++;
+        return contador;
+    };
+}
+
+const contador1 = criaContador(); // Closure 1
+const contador2 = criaContador(); // Closure 2
+
+console.log(contador1());
+console.log(contador1());
+console.log(contador1());
+console.log(contador1());
+
+console.log(contador2());
+console.log(contador2());
+console.log(contador2());
+console.log(contador2());
