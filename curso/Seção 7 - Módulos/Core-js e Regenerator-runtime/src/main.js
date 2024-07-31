6b3c5236c36668f2e0c5474912bc7da723379d6c
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-07-31 20:26:53 -0300

    Garantindo que a aplicacao rode em navegadores antigos com Core-js e regenerator-runtime

---
+++ curso/Seção 7 - Módulos/Core-js e Regenerator-runtime/src/main.js
@@ -0,0 +1,8 @@
+import "core-js/stable"; // Para rodar a aplicação em navegadores mais antigos
+import "regenerator-runtime/runtime"; // Para rodar a aplicação em navegadores mais antigos
+
+import executa from "./modules/promises";
+
+import "./assets/css/style.css";
+
+executa();
