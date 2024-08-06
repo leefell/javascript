c1a715a7a9e903858f4ae044f710cabab2a956e3
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-08-06 14:33:53 -0300

    nodemon

---
+++ curso/Seção 8 - Node/Server Express/Aula 2 - Express e Nodemon/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+// http://meusite.com/ <- GET -> Entregue a página /
+// http://meusite.com/sobre <- GET -> Entregue a página /sobre
+
+// app.operacao("rota", (requisicao, resposta) => {
+//     ...
+// })
+
+app.get("/", (req, res) => {
+  res.send(`
+    <form action="/" method="POST">
+    Nome do cliente: <input type="text" name="nome">
+    <button>Enviar formulário</button>
+    </form>
+    `);
+});
+
+app.post("/", (req, res) => {
+  res.send("Recebi o formulário.");
+});
+
+app.get("/contato", (req, res) => {
+  res.send("Chama no ZAP");
+});
+
+app.listen(3000, () => {
+  console.log("Acessar http://localhost:3000");
+  console.log("Servidor escutando na porta 3000");
+});
