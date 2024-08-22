d413ff0e667be73a0dbcfe0d14fb48cf58a05723
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-08-21 21:12:45 -0300

    Middleware e Verificacao de JWT

---
+++ curso/ApiRest/src/middlewares/loginRequired.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+
+export default (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({
+      errors: ["Login required"],
+    });
+  }
+
+  const [, token] = authorization.split(" ");
+
+  try {
+    const dados = jwt.verify(token, process.env.TOKEN_SECRET);
+
+    // Atrelando os dados já na requisição (id e email)
+    // qualquer página que passar por esse middleware.
+    const { id, email } = dados;
+    req.userId = id;
+    req.userEmail = email;
+
+    return next();
+  } catch (e) {
+    return res.status(401).json({
+      errors: ["Token expirado ou inválido."],
+    });
+  }
+};
