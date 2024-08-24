2a4a35e9a3e1dff43f4ea6e816ef714cfc575d1f
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-08-24 16:38:53 -0300

    Tratando arquivos enviados e delegando trabalho para FotoController

--- curso/ApiRest/src/controllers/FotoController.js
+++ curso/ApiRest/src/controllers/FotoController.js
@@ -1,10 +1,19 @@
+import multer from "multer";
+import multerConfig from "../config/multerConfig";
+
+const upload = multer(multerConfig).single("foto");
+
 class FotoController {
   async store(req, res) {
-    try {
-      res.json(req.file);
-    } catch (e) {
-      res.json("Não foi possível enviar a foto.");
-    }
+    return upload(req, res, (err) => {
+      if (err) {
+        return res.status(400).json({
+          errors: [err.code],
+        });
+      }
+
+      return res.json(req.file);
+    });
   }
 }
 
--- curso/ApiRest/src/routes/fotoRoutes.js
+++ curso/ApiRest/src/routes/fotoRoutes.js
@@ -1,14 +1,9 @@
 import { Router } from "express";
-import multer from "multer";
 
 import fotoController from "../controllers/FotoController";
-import multerConfig from "../config/multerConfig";
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
-// Receber um único arquivo upload.single(); habilita req.file
-router.post("/", upload.single("foto"), fotoController.store);
+router.post("/", fotoController.store);
 
 export default router;
