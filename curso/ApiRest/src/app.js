f6f0ae78517a1e2391bcb9fa0f611589c2922890
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-09-03 20:06:00 -0300

    CORS

--- curso/ApiRest/src/app.js
+++ curso/ApiRest/src/app.js
@@ -4,12 +4,26 @@ dotenv.config();
 
 import "./database";
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
 
 import homeRoutes from "./routes/homeRoutes";
 import userRoutes from "./routes/userRoutes";
 import tokenRoutes from "./routes/tokenRoutes";
 import alunoRoutes from "./routes/alunoRoutes";
 import fotoRoutes from "./routes/fotoRoutes";
+
+const whiteList = ["http://localhost:3000", "http://localhost:3001"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -19,6 +33,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(
