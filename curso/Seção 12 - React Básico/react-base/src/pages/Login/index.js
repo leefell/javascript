42237c14a13b9ce9a21bf99e2ea8771f62914400
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-09-10 20:51:02 -0300

    Configurando o axios

--- curso/Seção 12 - React Básico/react-base/src/pages/Login/index.js
+++ curso/Seção 12 - React Básico/react-base/src/pages/Login/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragrafo } from './styled';
 
+import axios from '../../services/axios';
+
 export default function Login() {
   return (
     <Container>
