421757967c69ab9c707567ace245dc564bbe0caf
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-12-29 11:35:32 -0300

    Consumindo a API de filmes com axios

--- react/fellflix/src/pages/Home/index.js
+++ react/fellflix/src/pages/Home/index.js
@@ -1,4 +1,25 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+
 function Home() {
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function getMovies() {
+      const response = await api.get("movie/now_playing", {
+        params: {
+          api_key: apiKey,
+          language: "pt-BR",
+          page: 1,
+        },
+      });
+      console.log(response.data.results);
+    }
+
+    getMovies();
+  }, []);
+
   return (
     <div>
       <h1>Bem vindo A Home</h1>
