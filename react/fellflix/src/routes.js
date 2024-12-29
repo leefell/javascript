46c6e9dd7f501e3a8a1693fe8bd261816d3f5e84
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-12-29 11:07:49 -0300

    header

--- react/fellflix/src/routes.js
+++ react/fellflix/src/routes.js
@@ -4,9 +4,12 @@ import Home from "./pages/Home";
 import Filme from "./pages/Filme";
 import Err from "./pages/Err";
 
+import Header from "./components/Header";
+
 function RoutesApp() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/filme/:id" element={<Filme />} />
