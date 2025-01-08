8f0e3b5ad3bd61c528a5a74d1492d23314566ea7
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2025-01-08 19:35:07 -0300

    revisando middlewares

--- nodejs/index.js
+++ nodejs/index.js
@@ -9,23 +9,50 @@ server.use(express.json());
 
 const cursos = ["Java", "Js", "react"];
 
+// Middleware Global
+server.use((req, res, next) => {
+  console.log(`URL chamada: ${req.url}`);
+
+  return next(); // passa pelo middleware e nao trava nele
+});
+
+// Esse é um middleware especifico que verifica se o nome do curso foi enviado
+function checkCurso(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({ error: "Nome do curso é obrigatório" });
+  }
+
+  return next(); // se passar no if, o fluxo continua normalmente
+}
+
+function checkIndexCurso(req, res, next) {
+  const index = req.params.index;
+  if (!cursos[index]) {
+    return res.status(400).json({ error: "O curso não existe" });
+  }
+
+  return next();
+}
+
 server.get("/cursos", (req, res) => {
   return res.json(cursos);
 });
 
-server.get("/curso/:index", (req, res) => {
+server.get("/curso/:index", checkIndexCurso, (req, res) => {
   const { index } = req.params;
 
   return res.json(cursos[index]);
 });
 
-server.post("/cursos", (req, res) => {
+// Usando o middleware na requisicao
+// localhost/cursos -> checkCurso -> postaCursoNovo
+server.post("/cursos", checkCurso, (req, res) => {
   const { name } = req.body;
   cursos.push(name);
   return res.json(cursos);
 });
 
-server.put("/cursos/:index", (req, res) => {
+server.put("/cursos/:index", checkCurso, checkIndexCurso, (req, res) => {
   const { index } = req.params;
   const { name } = req.body;
 
@@ -33,7 +60,7 @@ server.put("/cursos/:index", (req, res) => {
   return res.json(cursos);
 });
 
-server.delete("/cursos/:index", (req, res) => {
+server.delete("/cursos/:index", checkIndexCurso, (req, res) => {
   const { index } = req.params;
 
   cursos.splice(index, 1);
