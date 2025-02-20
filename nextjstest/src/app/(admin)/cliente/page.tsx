ebea4b18e731cdd95f42c04c6fd07267f8a2f57f
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2025-02-20 15:02:32 -0300

    grupo de rota

---
+++ nextjstest/src/app/(admin)/cliente/page.tsx
@@ -0,0 +1,8 @@
+export default function Cliente() {
+  return (
+    <div>
+      <h1>Cliente</h1>
+      <span>Seja bem-vindo a cliente</span>
+    </div>
+  );
+}
--- nextjstest/src/app/contatos/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Contatos(){
-    return(
-        <div>
-            <h1>Contatos</h1>
-            <span>Seja bem-vindo a contatos</span>
-        </div>
-    )
-}
